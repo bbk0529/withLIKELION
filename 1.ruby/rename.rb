d73d49effcf27f345e9b20f0fb1f19138fceb1b9
HerrPark <bbk0529@gmail.com>
2017-11-06 08:54:02 +0000

    rename

--- 1.ruby/rename.rb
+++ 1.ruby/rename.rb
@@ -5,22 +5,16 @@ Dir.chdir("src")
 #2. 폴더 안을 돌면서 파일들의 이름을 가져온다. 
 files =[]
 #3. files 배열 안에 바꾸고자 하는 모든 파일의 이름을 넣는다. 
-files = Dir.entries(Dir.pwd).reject  {|name|name[0]=='.'}
-puts files
-files.each do |f|
-    File.rename(f, "BK_" + f)
-end
+    # files = Dir.entries(Dir.pwd).reject  {|name|name[0]=='.'}
+    # puts files
+    # files.each do |f|
+    #     File.rename(f, "BK_" + f)
+    # end
 #path = "/workspace/ruby/1.ruby/src/"
 #3. 각각의 이름을 "1.TXT " --> "BKINTERNATIONAL.TXT"
 
 # #MY SOLUTION
-# puts Dir.pwd
-# Dir.chdir("src")
-
-# path = Dir.pwd
-# Dir.open(path).each do |f|
-#     next if f=="."  || f==".."
-#     newname = "BK INTERNATIONAL_" + f.to_s
-#     puts newname
-#     File.rename(f, newname) 
-# end
+Dir.open(Dir.pwd).each do |f|
+  next if f=="."  || f==".."
+  File.rename(f, "BK_" + f)
+end
